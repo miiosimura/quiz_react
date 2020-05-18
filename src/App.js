f93e4f288648cf9849173e45be77e1442b0f6685
mirian iosimura <mirian@blumpa.com>
2020-05-18 11:13:21 -0300

    Adicionando Semantic UI no projeto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Inicio from './home/Inicio'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Jogo de Perguntas e Respostas
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Inicio />
       </header>
     </div>
   );
---
+++ src/home/Inicio.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Container, Header, Segment, Card, Button } from 'semantic-ui-react'
+
+const Inicio = props => {
+    return (
+        <div>
+            <Container>
+                <Segment pilled>
+                    Quiz React
+                </Segment>
+                <Header as='h2'>
+                    Jogo de Perguntas e Respostas
+                </Header>
+                <p>
+                    Desafie os seus amigos neste incrível jogo de perguntas e respostas
+                </p>
+                <Card fluid>
+                    <Card.Content>
+                        Acesse agora mesmo!
+                    </Card.Content>
+                    <Card.Content>
+                        <Button basic color='blue'>
+                            Login com Facebook
+                        </Button>
+                        <Button basic color='green'>
+                            Usuário Administrativo
+                        </Button>
+                    </Card.Content>
+                </Card>
+            </Container>
+        </div>
+    )
+}
+
+export default Inicio
