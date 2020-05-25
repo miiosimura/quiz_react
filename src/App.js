dd8b328ce31cde61f0217de1bcd3f4bcebde37d6
mirian iosimura <mirian@blumpa.com>
2020-05-25 11:38:26 -0300

    Adicionando as rotas

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import {BrowserRouter, Route} from 'react-router-dom'
 
 import Inicio from './home/Inicio'
 import Categorias from './jogo/Categorias'
 import Perguntas from './jogo/Perguntas'
 import Resultado from './jogo/Resultado'
 import Ranking from './jogo/Ranking'
+import Header from './jogo/Header'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Ranking />
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <header className="App-header">
+            <Header />
+          </header>
+          <Route path='/' exact component={Inicio}></Route>
+          <Route path='/categorias' component={Categorias}></Route>
+          <Route path='/perguntas' component={Perguntas}></Route>
+          <Route path='/resultado' component={Resultado}></Route>
+          <Route path='/ranking' component={Ranking}></Route>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
