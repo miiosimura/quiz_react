f704aef2da0854ae2137e23eda125e3d44276787
mirian iosimura <mirian@blumpa.com>
2020-05-25 11:55:29 -0300

    Criacao da barra de menu

--- src/jogo/Header.js
+++ src/jogo/Header.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import {Menu, Image} from 'semantic-ui-react'
 
 const Header = props => {
     return (
-        <h1>Navegação</h1>
+        <Menu>
+            <Menu.Item as={Link} to='/'>Home</Menu.Item>
+            <Menu.Item as={Link} to='/categorias'>Categorias</Menu.Item>
+            <Menu.Item as={Link} to='/perguntas'>Perguntas</Menu.Item>
+            <Menu.Item as={Link} to='/resultado'>Resultado</Menu.Item>
+            <Menu.Item as={Link} to='/ranking'>Ranking</Menu.Item>
+            <Menu.Menu position='right'>
+                <Menu.Item><Image avatar src=''/>Fulano</Menu.Item>
+            </Menu.Menu>
+        </Menu>
     )
 }
 
