1054a4fc27ebb8eff5303f840c92cdbd24e6c9cb
mirian iosimura <mirian@blumpa.com>
2020-06-08 17:26:09 -0300

    Adicionando o valor da pontuacao final

--- src/jogo/Resultado.js
+++ src/jogo/Resultado.js
@@ -4,23 +4,24 @@ import {List, Container} from 'semantic-ui-react'
 import Resposta from './Resposta'
 import Navegacao from './Navegacao'
 
-const resposta = {
-  id: 1,
-  titulo: 'Pergunta 1',
-  alternativa: 3
-}
-
 class Resultado extends Component {
     render(){
+        const {resultado} = this.props.location.state
         return (
           <div>
               <Navegacao />
               <h2>Seus resultados</h2>
               <p>Confira seu desempenho nesta categoria:</p>
-
               <Container>
                   <List divided>
-                      <Resposta resposta={resposta}/>
+                    {
+                        Object.keys(resultado.resultado)
+                            .map(key => {
+                                return <Resposta pergunta={resultado[key].pergunta}
+                                                 resposta={resultado[key].resposta}
+                                                 key={key}/>
+                            })
+                    }
                   </List>
               </Container>
           </div>
