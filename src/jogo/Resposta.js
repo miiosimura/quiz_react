1f0a4d6f5ebe0ec80435870f826203a6f560a8fb
mirian iosimura <mirian@blumpa.com>
2020-06-15 10:26:02 -0300

    Exibindo o resultado e informando se o jogador acertou ou nao as perguntas

--- src/jogo/Resposta.js
+++ src/jogo/Resposta.js
@@ -2,12 +2,16 @@ import React from 'react'
 import {List, Label, Icon} from 'semantic-ui-react'
 
 const Resposta = props => {
-    const {pergunta, resposta, key} = props
+    const {pergunta, resposta, acertou, key} = props
     return (
         <List.Item key={key}>
             <List.Content floated='left'>
                 <Label>*</Label>
                 {pergunta}
+            </List.Content>
+            <List.Content floated='right'>
+                { acertou && <Icon name='thumbs up outline'/> }
+                { !acertou && <Icon name='thumbs down outline'/> }
             </List.Content>
             <List.Content floated='right'>
                 {resposta}
@@ -17,6 +21,3 @@ const Resposta = props => {
 }
 
 export default Resposta
-
-//thumbs up outline
-//thumbs down outline
--- src/jogo/Resultado.js
+++ src/jogo/Resultado.js
@@ -4,26 +4,36 @@ import {List, Container} from 'semantic-ui-react'
 import Resposta from './Resposta'
 import Navegacao from './Navegacao'
 
+let label = 'pontos'
+
 class Resultado extends Component {
     render(){
-        const {resultado} = this.props.location.state
+        const {resultado, pontos} = this.props.location.state
+
+        if (pontos == 1){
+            label = 'ponto'
+        }
         return (
           <div>
               <Navegacao />
               <h2>Seus resultados</h2>
-              <p>Confira seu desempenho nesta categoria:</p>
+              <p>Confira seu desempenho:</p>
               <Container>
                   <List divided>
                     {
-                        Object.keys(resultado.resultado)
-                            .map(key => {
-                                return <Resposta pergunta={resultado[key].pergunta}
-                                                 resposta={resultado[key].resposta}
-                                                 key={key}/>
+                        Object.keys(resultado)
+                        .map(key => {
+                            return (
+                                <Resposta pergunta={resultado[key].pergunta}
+                                resposta={resultado[key].resposta}
+                                acertou={resultado[key].acertou}
+                                key={key}/>
+                                )
                             })
-                    }
+                        }
                   </List>
               </Container>
+              <h3>Total: {pontos} {label}</h3>
           </div>
         )
     }
